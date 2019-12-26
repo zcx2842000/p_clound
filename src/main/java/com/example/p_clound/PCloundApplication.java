3f12b892d0fcb954e7f32bc53b8a25c02dd2c39f
luoshudong <luoshudong@lsd>
2019-12-26 19:33:04 +0800

    spring_cloud:eureka_demo

---
+++ src/main/java/com/example/p_clound/PCloundApplication.java
@@ -0,0 +1,13 @@
+package com.example.p_clound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCloundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PCloundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/p_clound/PCloundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p_clound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PCloundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
